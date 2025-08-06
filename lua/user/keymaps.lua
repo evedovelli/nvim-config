4a2dfef80d5e37c3b7404f2e7aef3b06a98fb7fe
Estevan Vedovelli <Estevan.Vedovelli@amd.com>
2025-08-06 09:52:00 -0400

    Update keymaps for Copilot (ALT-<directional>)

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -111,6 +111,6 @@ keymap("n", "<leader>P", '"+P', opts)
 
 -- Copilot
 vim.g.copilot_no_tab_map = true
-keymap('i', '<leader>k', 'copilot#Accept("\\<CR>")', {expr=true, replace_keycodes = false})
-keymap('i', '<leader>l', '<Plug>(copilot-accept-word)')
-keymap('i', '<leader>j', '<Plug>(copilot-accept-line)')
+keymap('i', '<A-k>', 'copilot#Accept("\\<CR>")', {expr=true, replace_keycodes = false})
+keymap('i', '<A-l>', '<Plug>(copilot-accept-word)')
+keymap('i', '<A-j>', '<Plug>(copilot-accept-line)')
